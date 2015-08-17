f9b3643b5c0d1da1b02f1f5715ac86479f95bf37
Sigit Dewanto <sigitdewanto@gmail.com>
2015-08-17 17:09:01 +0700

    Remove my machine specific credentials.

--- sil2ah/core-settings.php
+++ sil2ah/core-settings.php
@@ -1,10 +1,9 @@
 <?php
 if (!defined('ON_ROOT')) { include 'views/404.php'; die();}
 
-define("DB_HOST", "127.0.0.1");
-define("DB_USER", "root");
-define("DB_PASSWORD", "s34g4t3s0ft");
-define("DB_NAME", "sil2ah");
+define("DB_HOST", "");
+define("DB_USER", "");
+define("DB_PASSWORD", "");
+define("DB_NAME", "");
 
 define("DEBUGGING", false);
-define("PASSWORD_SALT", "nvjNOBE3HQ49RJiIf4W84INJnJBFbBHB8349783BJfh08H4");
